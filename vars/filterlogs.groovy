bc1b56e209fdb002be6af280b92276641a1bac94
atulCapgemini <163232500+atulCapgemini@users.noreply.github.com>
2025-06-24 22:19:37 +0530

    Update filterlogs.groovy

--- vars/filterlogs.groovy
+++ vars/filterlogs.groovy
@@ -1,7 +1,7 @@
 def call(String filter_string, int occurrence) {
-    def logs = currentBuild.rawBuild.getLog(10000).join('\n')
-    int count = logs.count(filter_string)
-    if (count > occurrence - 1) {
+ def logs = currentBuild.rawBuild.getLog(10000).join('\n')
+ int count = logs.count(filter_string)
+   if (count > occurrence - 1) {
         currentBuild.result = "UNSTABLE"
     }
 }
