54aa9a78982cad6e9508438dc517caeacacf037c
atulCapgemini <163232500+atulCapgemini@users.noreply.github.com>
2025-06-24 19:41:03 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,16 @@
-def call(){
+def call(String mvnaction){
+  if(mvnaction == "clean"){
+    sh "mvn clean"
+  }
+  else if(mvnaction == "compile"){
+    sh "mvn clean compile"
+  }
   
+  else if(mvnaction == "clean"){
+    sh "mvn clean test
+  }
+  
+  else if(mvnaction == "install"){
+    sh "mvn clean install"
+  }
 }
